5e0fb88d797465fa6326e02c4be46b33a3678e16
Benjamin Björk <bb222bx@student.lnu.se>
2015-10-27 23:05:57 +0100

    fix

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once("controller/MasterController.php");
 require_once("model/FileModel.php");
 require_once("view/View.php");
 require_once("view/UploadView.php");
-require_once("view/Upload.php");
+require_once("view/upload.php");
 
 $up = new \view\UploadView();
 $v = new view\View();
