52e3b606df112f64fde91886ead1089182c004b4
Benjamin Björk <bb222bx@student.lnu.se>
2015-10-28 00:49:17 +0100

    test

--- model/Database_config.php
+++ model/Database_config.php
@@ -14,7 +14,7 @@ namespace model;
      $dbname = \Settings::DB_name;
      $dbtabell = \Settings::DB_tabel;
 
-    mysqli_connect($dbhost,$dbuser,$dbpass) or die('Cannot connect to server');
-    mysqli_select_db($dbname,$dbtabell);
+    mysql_connect($dbhost,$dbuser,$dbpass) or die('Cannot connect to server');
+    mysql_select_db($dbname,$dbtabell);
 
 
