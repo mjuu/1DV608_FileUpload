809ffa3657eb0f0c454b94d76ce2857c5d98f72d
Benjamin Björk <bb222bx@student.lnu.se>
2015-12-02 21:03:23 +0100

    implemented get private file list

--- model/DBConn.php
+++ model/DBConn.php
@@ -8,6 +8,12 @@
 namespace model;
 class DBConn{
     private $pdo;
+
+    /**
+     * Making a connection to the database
+     * and returns the connection
+     * @return \PDO
+     */
     public function conn(){
         $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAMEFILE;
         try {
@@ -18,12 +24,26 @@ class DBConn{
         }
     }
 
+    /**
+     * Get public file list
+     * @return array of the table
+     */
     public function getPublicFileList(){
 
         $sql = "SELECT * FROM ".DB_TABELLFILE;
         $query = $this->pdo->prepare($sql);
         $query->execute();
        return $results = $query->fetchAll();
+    }
+    public function getPrivateFileList(){
+
+        $username1 =$_SESSION['user'];
+        $sql = "SELECT * FROM ". DB_TABELLFILEPRIVATE ." WHERE username = :username";
+        //$sql = "SELECT * FROM private_file_uploads WHERE username = :username";
+        $query = $this->pdo->prepare($sql);
+        $query->bindParam(':username',$username1);
+        $query->execute();
+        return $results = $query->fetchAll();
     }
 
 }
